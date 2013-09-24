199721e774cf0c2299222387acb217554112e806
Bob Cribbs <bcribbs@zehnergroup.com>
2013-09-24 13:49:21 +0300

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+# cookbook-lampdemo cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'cookbook-lampdemo'
+maintainer       'Bob Cribbs'
+maintainer_email 'foo@example.com'
+license          'All rights reserved'
+description      'Installs/Configures cookbook-lampdemo'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
